aafd3f6ec1c390863153a29d7cd925e559624427
Avindiamanta <avinophotography@gmail.com>
2015-09-30 10:54:39 +0700

    Initial Commit

---
+++ City Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  City Map
+//
+//  Created by Tiffany Diamanta on 9/30/15.
+//  Copyright © 2015 Tiffany Diamanta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
